6167ff3d004e7926ef53ba429d99191b90c4b88c
L Carver <wifihum@gmail.com>
2023-02-20 18:03:04 -0800

    add file and function comment headers

--- source/app.cpp
+++ source/app.cpp
@@ -1,21 +1,39 @@
+//------------------------------------------------------------------------------
+// app.cpp
+// 
+// Displays a list of random numbers in forward order, then in backward order.
+// Random numbers range from 1 to user's maximum n, inclusive.
+//------------------------------------------------------------------------------
 #include <iostream>
 #include <cstdlib>
 #include <vector>
 
+//------------------------------------------------------------------------------
+// using symbols
+//------------------------------------------------------------------------------
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
 
+//------------------------------------------------------------------------------
+// constants
+//------------------------------------------------------------------------------
 constexpr int NUM_RANDOMS = 10;
 int max = 1;
 vector<int> vi;
 
+//------------------------------------------------------------------------------
+// local function prototypes
+//------------------------------------------------------------------------------
 void getRange();
 void makeList();
 void displayForwards();
 void displayBackwards();
 
+//------------------------------------------------------------------------------
+// entry point
+//------------------------------------------------------------------------------
 int main() {
 
 	cout << "\nDisplays a list of " << NUM_RANDOMS
@@ -39,12 +57,18 @@ int main() {
 	return 0;
 }
 
+//------------------------------------------------------------------------------
+// updates global int max
+//------------------------------------------------------------------------------
 void getRange() {
 	cout << "\nEnter n, your max random number (0 quits): ";
 	cin >> max;
 	max = abs(max);
 }
 
+//------------------------------------------------------------------------------
+// fills vector with NUM_RANDOMS random numbers from 1 to max, inclusive
+//------------------------------------------------------------------------------
 void makeList() {
 
 	for (int i = 0; i < NUM_RANDOMS; i++) {
@@ -52,6 +76,9 @@ void makeList() {
 	}
 }
 
+//------------------------------------------------------------------------------
+// traverses vector in forward order to display its elements
+//------------------------------------------------------------------------------
 void displayForwards() {
 
 	for (int i = 0; i < NUM_RANDOMS; i++) {
@@ -60,6 +87,9 @@ void displayForwards() {
 	cout << endl;
 }
 
+//------------------------------------------------------------------------------
+// traverses vector in backwards order to display its elements
+//------------------------------------------------------------------------------
 void displayBackwards() {
 
 	while (!vi.empty()) {
